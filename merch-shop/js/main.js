a467f5a909cb840037a46c66e550d3d4f9e12ccd
milkndcoffee <milkndcoffee@gmail.com>
2020-05-03 19:46:52 -0500

    mobile ver so far: edited shop so that it has its own nav bar

--- merch-shop/js/main.js
+++ merch-shop/js/main.js
@@ -13,9 +13,36 @@ function openBar() {
             navBar.style.width = "15%";
         }
     }
-    
 }
 
 function closeBar() {
-  document.getElementById("nav-id").style.width = "0";
+    document.getElementById("nav-id").style.width = "0";
 }
+
+//SHOP
+function openShopNav(){
+    shopNav = document.getElementById("shop-nav");
+    shopButton = document.getElementById("shop-button-id");
+    if (shopButton.classList.contains("active-shop-button")){
+        shopButton.classList.remove("active-shop-button");
+        shopNav.style.height = "0";
+        shopNav.style.opacity = "0";  
+    } else{ 
+        shopNav.style.height = "30%";
+        shopNav.style.opacity = "1";
+        shopButton.classList.add("active-shop-button");
+    }
+    
+    
+    //document.getElementById("shop-button-id").src = "https://img.icons8.com/android/48/000000/collapse-arrow.png";
+}
+
+/*might use, might not use
+function pushUpAndFade(){
+    title = document.getElementById("mobile-title-id");
+    //title.style.height = "0";
+    title.style.opacity = "0";
+    title.style.overflow = "hidden";
+    title.style.height = "0";
+    //title.classList.add("title-transition");
+}*/
