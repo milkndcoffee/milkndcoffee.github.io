70cb87418f1351c3af48b5cfc00e62aef157d226
milkndcoffee <milkndcoffee@gmail.com>
2020-05-26 16:29:16 -0500

    pushing database for testing +fixing spacing in NaRSS

--- merch-shop/js/main.js
+++ merch-shop/js/main.js
@@ -1,48 +1,42 @@
 //NAVBAR
 function openBar() {
-    navBar = document.getElementById("nav-id");
-    if (screen.width <= 769){
-        navBar.style.width = "80%";
+  navBar = document.getElementById("nav-id");
+  if (screen.width <= 769) {
+    navBar.style.width = "80%";
+  } else {
+    //to allow reclicking on the navbar button to open and close
+    if (navBar.classList.contains("active-bar")) {
+      navBar.classList.remove("active-bar");
+      closeBar();
     } else {
-        //to allow reclicking on the navbar button to open and close
-        if (navBar.classList.contains("active-bar")){
-            navBar.classList.remove("active-bar");
-            closeBar();
-        } else {
-            navBar.classList.add("active-bar");
-            navBar.style.width = "15%";
-        }
+      navBar.classList.add("active-bar");
+      navBar.style.width = "15%";
     }
+  }
 }
 
 function closeBar() {
-    document.getElementById("nav-id").style.width = "0";
+  document.getElementById("nav-id").style.width = "0";
 }
 
-//SHOP
-function openShopNav(){
-    shopNav = document.getElementById("shop-nav");
-    shopButton = document.getElementById("shop-button-id");
-    if (shopButton.classList.contains("active-shop-button")){
-        shopButton.classList.remove("active-shop-button");
-        shopNav.style.height = "0";
-        shopNav.style.opacity = "0";  
-    } else{ 
-        shopNav.style.height = "30%";
-        shopNav.style.opacity = "1";
-        shopButton.classList.add("active-shop-button");
-    }
-    
-    
-    //document.getElementById("shop-button-id").src = "https://img.icons8.com/android/48/000000/collapse-arrow.png";
+//SHOP NAVBAR
+function openShopNav() {
+  shopNav = document.getElementById("shop-nav");
+  shopButton = document.getElementById("shop-button-id");
+  if (shopButton.classList.contains("active-shop-button")) {
+    shopButton.classList.remove("active-shop-button");
+    shopNav.style.height = "0";
+    shopNav.style.opacity = "0";
+  } else {
+    shopNav.style.height = "30%";
+    shopNav.style.opacity = "1";
+    shopButton.classList.add("active-shop-button");
+  }
 }
 
-/*might use, might not use
-function pushUpAndFade(){
-    title = document.getElementById("mobile-title-id");
-    //title.style.height = "0";
-    title.style.opacity = "0";
-    title.style.overflow = "hidden";
-    title.style.height = "0";
-    //title.classList.add("title-transition");
-}*/
+//-- /shop/.. functions
+function getItemData(data){
+  fetch(data)
+    .then(response => response.json())
+    .then(json => console.log(json));
+}
