ee5725ad0b81fbf5a63be614ac59d96c07dd2033
milkndcoffee <milkndcoffee@gmail.com>
2020-06-12 17:35:05 -0500

    finished appending functions from json to js to dom to html

--- merch-shop/js/main.js
+++ merch-shop/js/main.js
@@ -1,9 +1,14 @@
 /*TODO: 
-  [6/6]function appendSectionData : add onto body multiple DOM elements to <section>.
+  
   [6/8] function getItemData() : do the rest for other clothing items
-  [6/8] html data function - finish appenSectionData function so that iit displays the fetched json file content.
+  [6/12] fixed appended DOM JSON data spacing.
+  [6/12] (low priority) create images for each item.
+  
 
+  DONE [6/12]function appendSectionData : add onto body multiple DOM elements to <section>.
+  DONE [6/12] html data function - finish appenSectionData function so that iit displays the fetched json file content.
   DONE [6/8]:: function getItemData (6/6): add existing data onto to existing array type
+  
 */
 
 //NAVBAR
@@ -42,70 +47,15 @@ function openShopNav() {
   }
 }
 
-/*
-  create a template that will take in several information in regards to
-  item article, so that each data broken down can be appended onto
-  the <main> element within the html.
-
-  <section class="clothing-articles">
-    <header><h2 id="item-h2"></h2></header>
-    <figure>
-      <img id="item-img"/>
-      <figcaption id="item-figcaption"></figcaption>
-    </figure>
-  </section>
-*/
-function appendSectionData(sectionId, itemTitle, itemImg, itemName, itemPrice) {
-
-  //element initialization
-  var sectionElement = document.createElement("section");
-  var headerElement = document.createElement("header");
-  var h2Element = document.createElement("h2");
-  var figureElement = document.createElement("figure");
-  var imgElement = document.createElement("img");
-  var figcaptionElement = document.createElement("figcaption");
-
-  //node construction/initialization
-  //template
-  var itemTitle = itemTitle;
-  var itemImg = itemImg;
-  var itemName = itemName;
-  var itemPrice = itemPrice;
-  var itemTest = "test";
-  var itemCap = itemName + itemPrice;
-
-
-  //applying the nodes to the objects
-  //imgElement.src = "../template_img.src";
-  sectionElement.className = "clothing-articles";
-  imgElement.className = "placeholder-clothing-piece";
-  imgElement.src = "not-found.png";
-  h2Element.innerText = itemTest;
-  figcaptionElement.innerHTML = itemTest;
-
-  //constructing the elements together
-  var mainInBody = document.getElementById("main-accs");
-  mainInBody.appendChild(sectionElement);
-  sectionElement.appendChild(headerElement);
-  headerElement.appendChild(h2Element);
-  sectionElement.appendChild(figureElement);
-  figureElement.appendChild(imgElement);
-  figureElement.appendChild(figcaptionElement);
 
 
 
-}
-
-
-//-- /shop/.. functions
+/*    ------------------
+      /shop/.. functions
+      ------------------    */    
 function getItemData(x) {
-
-  //dom testing
-
-  //declaration of variables
-  let accessories = "";
   if (x == "accessories") {
-    let accessoriesData = "";
+    let accsData = "";
     var accs = "";
     data = "https://milkndcoffee.github.io/merch-shop/db/clothes-db.json"
     fetch(data)
@@ -114,8 +64,8 @@ function getItemData(x) {
         var jsonData = json;
         var jsonString = JSON.stringify(json.product[2]);
         accs = jsonString;
-        accessoriesData = json.product[2];
-        console.log('accessories :', accessoriesData)
+        accsData = json.product[2];
+        console.log('accessories JSON :', accsData)
 
         var count = 1;
         var beanieC = 0;
@@ -127,16 +77,16 @@ function getItemData(x) {
           breaking apart the json file data so that we can section the data based on
           the 'type' of the 'accessories' article.
         */
-        for (i in accessoriesData.items) {
-          if (accessoriesData.items[i].type in accsTypeObj) {
+        for (i in accsData.items) {
+          if (accsData.items[i].type in accsTypeObj) {
             for (const typeProp in accsTypeObj) {
-              if (typeProp == accessoriesData.items[i].type) {
+              if (typeProp == accsData.items[i].type) {
                 var newObj = {
-                  id: accessoriesData.items[i].id,
-                  name: accessoriesData.items[i].name,
-                  description: accessoriesData.items[i].description,
-                  price: accessoriesData.items[i].price,
-                  imgSrc: accessoriesData.items[i].img
+                  id: accsData.items[i].id,
+                  name: accsData.items[i].name,
+                  description: accsData.items[i].description,
+                  price: accsData.items[i].price,
+                  imgSrc: accsData.items[i].img
                 };
                 accsTypeObj[typeProp].push(newObj);
               }
@@ -149,21 +99,22 @@ function getItemData(x) {
             */
             var typeArr = new Array();
             typeArr.push({
-              id: accessoriesData.items[i].id,
-              name: accessoriesData.items[i].name,
-              description: accessoriesData.items[i].description,
-              price: accessoriesData.items[i].price,
-              imgSrc: accessoriesData.items[i].img
+              id: accsData.items[i].id,
+              name: accsData.items[i].name,
+              description: accsData.items[i].description,
+              price: accsData.items[i].price,
+              imgSrc: accsData.items[i].img
             });
-            accsTypeObj[accessoriesData.items[i].type] = typeArr;
+            accsTypeObj[accsData.items[i].type] = typeArr;
           }
         } console.log("END RESULT:", accsTypeObj);
 
-        var article = JSON.stringify(json.product[2].article);
-        let clothes = JSON.stringify(json.product[2].items);
-        console.log(article);
-
-        console.log(clothes);
+        appendDataToBody(accsTypeObj, "accs");
+        
+        //var article = JSON.stringify(json.product[2].article);
+        //let clothes = JSON.stringify(json.product[2].items);
+        //console.log(article);
+        //console.log(clothes);
 
       });
     //console.log(accessories);
@@ -177,21 +128,79 @@ function getItemData(x) {
 
 }
 
-/*
-function getItemData() {
-  let accessories = "";
-  var accs = "";
-  data = "https://milkndcoffee.github.io/merch-shop/db/clothes-db.json"
-  fetch(data)
-    .then(response => response.json())
-    .then(json => {
-      var jsonString = JSON.stringify(json.product[2]);
-      accs = jsonString;
-      accessories = jsonString;
-      console.log(jsonString);
-      console.log(json.product[2]);
-      console.log(accessories);
-    });
-  console.log(accessories);
+function createFigureData(sectionId, itemImg, itemName, itemPrice) {
+  //element initialization
+  var figureElement = document.createElement("figure");
+  var imgElement = document.createElement("img");
+  var figcaptionElement = document.createElement("figcaption");
+
+  //node construction/initialization
+  var sectId = sectionId; //unused atm but might be utilized later on for creating an item popup.
+  var itemImg = itemImg;
+  var itemName = itemName;
+  var itemPrice = itemPrice;
+  var itemCap = itemName + " $" + itemPrice;
+
+  //applying the nodes to the objects
+  //imgElement.src = "../template_img.src";
+  imgElement.className = "placeholder-clothing-piece";
+  imgElement.src = "not-found.png";
+  figcaptionElement.innerHTML = itemCap;
+
+  //constructing the elements together
+  figureElement.appendChild(imgElement);
+  figureElement.appendChild(figcaptionElement);
+
+  return figureElement; //we are going to return this constructed dom element to use for appending towards their desired sects
 }
-*/
+
+function createSectionData(dataObj){
+  var mainInBody = document.getElementById("main-accs");
+  var sectElementArr = new Array();
+  var loopCount = 0;
+ 
+  //sectioning off based of type
+  for (const dataType in dataObj){
+    var tempSectEl = document.createElement("section");
+    var tempHeaderEl = document.createElement("header");
+    var tempH2El = document.createElement("h2");
+
+    //header construction
+    tempH2El.innerText = dataType.toLocaleLowerCase();
+    tempHeaderEl.appendChild(tempH2El);
+    tempSectEl.appendChild(tempHeaderEl);
+
+    //going of the amount of items in a type
+    for (i = 0; i< dataObj[dataType].length; i++){
+      //for each 'type' of this 'object' do this:
+
+      var figElement = createFigureData(
+        dataObj[dataType][i].id,
+        dataObj[dataType][i].imgSrc,
+        dataObj[dataType][i].name,
+        dataObj[dataType][i].price
+        );
+        
+      tempSectEl.appendChild(figElement);
+      tempSectEl.className = "clothing-articles";
+    }
+    
+    //storing it into the array we are going to be utilizing for appending
+    sectElementArr[loopCount] = tempSectEl;
+    loopCount++;
+    
+  }
+
+  return sectElementArr; //we are going to return an array of constructed DOM elements
+}
+
+function appendDataToBody(data, location){
+  if (location == 'accs'){
+    var mainInBody = document.getElementById("main-accs");
+    var constructedData = createSectionData(data);
+
+  for (i=0; i<constructedData.length; i++){
+    mainInBody.appendChild(constructedData[i]);
+    }
+  }
+}
